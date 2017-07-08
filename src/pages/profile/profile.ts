10eac1a5e6768dda7b649ac6159864657a062b92
hyesungko <kohs0429@gmail.com>
2017-07-08 05:05:16 -0700

    Add logout on profile page

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { AuthProvider } from '../../providers/auth/auth';
 /**
  * Generated class for the ProfilePage page.
  *
@@ -14,11 +14,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public authProvider: AuthProvider) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
+  logout(): void {
+    this.authProvider.logoutUser();
   }
-
 }
